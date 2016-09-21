f08e77d7825930a6959c012aa6d10a202884eed5
lighters <huangdiv5@gmail.com>
2016-09-21 15:56:32 +0800

    Add the drawer and simple network fetch.

--- index.android.js
+++ index.android.js
@@ -10,10 +10,27 @@ import {
   StyleSheet,
   DrawerLayoutAndroid,
   Text,
+  TextInput,
+  ToastAndroid,
   View
 } from 'react-native';
 
 class RnDemos extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {text: '', data: 'init data...'};
+    this.getUserData = this._getUserData.bind(this);
+  }
+
+  _getUserData(key){
+    fetch(`http://brucezz.leanapp.cn/${key}`)
+     .then( (response) => response.json())
+     //.then( (data) => console.log(data) )
+     .then( (json) => json.message)
+     .then( (json) => this.setState({data: json}))
+  }
+
   render() {
     var navigationView = (
       <View style={{flex: 1, backgroundColor: '#fff'}}>
@@ -23,12 +40,30 @@ class RnDemos extends Component {
     return (
       <DrawerLayoutAndroid
         drawerBackgroundColor="rgba(0,0,0,0.5)"
+        statusBarBackgroundColor="blue"
         drawerWidth = {300}
         drawerPosition={DrawerLayoutAndroid.positions.Left}
+        onDrawerClose={() => ToastAndroid.show("close", ToastAndroid.SHORT)}
+        onDrawerOpen={() => ToastAndroid.showWithGravity("open", ToastAndroid.LONG, ToastAndroid.TOP)}
         renderNavigationView={() => navigationView}>
-        <View style={{flex: 1, alignItems: 'center'}}>
-          <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>Hello</Text>
-          <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>World!</Text>
+        <View style={{padding: 10}}>
+          <Text>The Text Translater....</Text>
+          <TextInput
+            style={{height: 40}}
+            placeholder="Type here to translate!"
+            onChangeText={(text) => this.setState({text})}
+          />
+          <Text style={{padding: 10, fontSize: 42}}>
+            {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+          </Text>
+          <Text>The Simple Network Fetch....</Text>
+          <TextInput
+            style={{height: 40, width: 200, borderColor: 'gray', borderWidth: 1}}
+            onSubmitEditing = {(text) => this.getUserData(text.nativeEvent.text)}
+            />
+          <Text style={styles.instructions}>
+            {this.state.data}
+          </Text>
         </View>
       </DrawerLayoutAndroid>
     );
